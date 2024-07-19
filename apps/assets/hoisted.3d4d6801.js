b905e366931f95ca736d43e6281239380210e2b7
William <willtheorangeguy@users.noreply.github.com>
2024-07-19 11:24:50 -0600

    docs: Update logo source URL to public path

--- apps/assets/hoisted.3d4d6801.js
+++
@@ -1,8 +0,0 @@
-import "https://www.google.com/recaptcha/api.js?render=6Ld-WwAiAAAAAKfIZkOlY0Y-RF5obrjrEiWI1TwC";
-grecaptcha.ready(async () => {
-  const e = await grecaptcha.execute(
-    "6Ld-WwAiAAAAAKfIZkOlY0Y-RF5obrjrEiWI1TwC",
-    { action: "homepage" },
-  );
-  document.querySelector("#captchaResponse").value = e;
-});
