cb3c9c9fc41588725a6de4581cef8f3b0d997e2b
William <willtheorangeguy@users.noreply.github.com>
2024-07-19 11:16:15 -0600

    docs(repos): Update repo configuration and remove unused workflows

---
+++ src/config.ts
@@ -0,0 +1,71 @@
+import type { Site, SocialObjects } from "./types";
+
+export const SITE: Site = {
+    website: "https://williamvdg.me/",
+    author: "William Vandergraaf",
+    desc: "A dev blog, with random personal thoughts, tech stuff, and travel musing.",
+    title: "To Be Continued...",
+    ogImage: "hero.jpg",
+    lightAndDarkMode: true,
+    postPerPage: 3,
+};
+
+export const LOCALE = ["en-EN"]; // set to [] to use the environment default
+
+export const LOGO_IMAGE = {
+    enable: false,
+    svg: true,
+    width: 216,
+    height: 46,
+};
+
+export const SOCIALS: SocialObjects = [
+    {
+        name: "Github",
+        href: "https://github.com/willtheorangeguy/",
+        linkTitle: `@willtheorangeguy on Github`,
+        active: true,
+    },
+    {
+        name: "Instagram",
+        href: "https://instagram.com/william.vdg/",
+        linkTitle: `@william.vdg on Instagram`,
+        active: true,
+    },
+    {
+        name: "LinkedIn",
+        href: "https://www.linkedin.com/in/william-v-38981428b/",
+        linkTitle: `as William Vandergraaf on LinkedIn`,
+        active: true,
+    },
+    {
+        name: "Mastodon",
+        href: "https://techhub.social/@willtheorangeguy",
+        linkTitle: `@willtheorangeguy on Mastodon`,
+        active: true,
+    },
+    {
+        name: "Twitter",
+        href: "https://x.com/williamvdg1",
+        linkTitle: `@williamvdg1 on Twitter`,
+        active: true,
+    },
+    {
+        name: "YouTube",
+        href: "https://www.youtube.com/@willtheorangeguy",
+        linkTitle: `@willtheorangeguy on YouTube`,
+        active: true,
+    },
+    {
+        name: "Discord",
+        href: "discord.com/lcskid",
+        linkTitle: `@lcskid on Discord`,
+        active: true,
+    },
+    {
+        name: "Steam",
+        href: "https://steamcommunity.com/id/lcskid/",
+        linkTitle: `@lcskid on Steam`,
+        active: true,
+    },
+];
