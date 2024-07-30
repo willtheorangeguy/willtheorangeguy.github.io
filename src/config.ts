34f55bdd5c976b7c3bb65431e07817724ce4fd70
William <willtheorangeguy@users.noreply.github.com>
2024-07-30 08:20:07 -0600

    fix(config): update postPerPage value from 3 to 5

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const SITE: Site = {
     title: "willtheorangeguy",
     ogImage: "hero.jpg",
     lightAndDarkMode: true,
-    postPerPage: 3,
+    postPerPage: 5,
 };
 
 export const LOCALE = ["en-EN"]; // set to [] to use the environment default
