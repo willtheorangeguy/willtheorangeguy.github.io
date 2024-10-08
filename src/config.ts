ed98daf35b2b79563579b5a4d7effb7d6cc284b4
William Vandergraaf <willtheorangeguy@users.noreply.github.com>
2024-10-08 15:07:42 -0600

    fix: limit posts per page

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const SITE: Site = {
     title: "willtheorangeguy",
     ogImage: "hero.jpg",
     lightAndDarkMode: true,
-    postPerPage: 5,
+    postPerPage: 10,
 };
 
 export const LOCALE = ["en-EN"]; // set to [] to use the environment default
