4877cc327ebe07f059161994849417866924c692
William <willtheorangeguy@users.noreply.github.com>
2025-06-05 06:39:22 -0700

    fix: update image responsiveness

--- astro.config.ts
+++ astro.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
   image: {
     // Used for all Markdown images; not configurable per-image
     // Used for all `<Image />` and `<Picture />` components unless overridden with a prop
-    experimentalLayout: "constrained",
+    experimentalLayout: "responsive",
   },
   experimental: {
     responsiveImages: true,
